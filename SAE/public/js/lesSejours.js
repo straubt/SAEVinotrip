f7b64b687bd32080327edadfc4936fc5f32f4c75
straubt <titouan.straub@etu.univ-smb.fr>
2022-12-02 13:24:02 +0000

    main ok

---
+++ SAE/public/js/lesSejours.js
@@ -0,0 +1,14 @@
+function create(tag, parent, text=null, classs=null, id=null) {
+    let element = document.createElement(tag)
+    if (text)
+      element.appendChild(document.createTextNode(text))
+    parent.appendChild(element)
+    if (classs)
+      element.classList.add(classs)
+    if (id)
+      element.id = id
+    return element
+  }
+
+var parent = document.querySelector(".parent");
+
