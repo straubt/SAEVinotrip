d39b4ea6ef2b448c0add18421558b95d56a586ac
straubt <titouan.straub@etu.univ-smb.fr>
2022-12-09 13:20:36 +0000

    ok Titouan

--- SAE/public/js/profile.js
+++ SAE/public/js/profile.js
@@ -17,13 +17,8 @@ let divImage = document.querySelector(".image");
 
 // creation de l'image
 var img = document.createElement("img");
-<<<<<<< HEAD
-img.id = "image"
-create("p", divImage, client.titre_client, null, null).id = "sexe";
-=======
 img.id="image"
 let sex = create("p", divImage, null, null, "sexe");
->>>>>>> main
 
 // --------- initialisation image par apport au sexe
 if (client.titre_client == "M") {
@@ -42,17 +37,6 @@ sex.appendChild(img);
 //regroupememnt infos
 let divInfo = document.querySelector(".infos");
 // creation info
-<<<<<<< HEAD
-create("p", divInfo, client.id_client, null, null).id = "id";
-create("p", divInfo, client.prenom_client, null, null).id = "prenom";
-create("p", divInfo, client.nom_client, null, null).id = "nom";
-create("p", divInfo, client.mail_client, null, null).id = "mail";
-create("p", divInfo, client.date_naiss_client, null, null).id = "date";
-
-let buttonModif = document.querySelector("#modification");
-
-buttonModif.addEventListener('click', function () {
-=======
 
 create("p", divInfo, client.prenom_client, null, "prenom");
 create("p", divInfo, client.nom_client, null, "nom");
@@ -63,7 +47,6 @@ let buttonModif = document.querySelector("#modification");
 
 
 buttonModif.addEventListener('click', function(){
->>>>>>> main
   //suppression des anciens p et remplacement par un formulaire
   let allP = document.querySelectorAll("p");
   let infos = document.querySelector(".infos");
@@ -80,51 +63,6 @@ buttonModif.addEventListener('click', function(){
   let form = create("form", infos, null, "parentModif", null );
   form.method = "post";
   form.action = 'profile';
-<<<<<<< HEAD
-  let input = create("input", form, null, null, null); //token form csrf
-  input.type = "hidden";
-  input.name = "_token";
-  input.value = csrf;
-  //label prenom
-  let labelPrenom = create("label", form, "Prenom :", null, null);
-  //input prenom
-  let inputPrenom = create("input", form, null, null, null);
-  inputPrenom.placeholder = client.prenom_client;
-  inputPrenom.name = "prenom";
-  inputPrenom.value = client.prenom_client;
-  //label nom
-  let labelNom = create("label", form, "Nom :", null, null);
-  //input nom
-  let inputNom = create("input", form, null, null, null);
-  inputNom.placeholder = client.nom_client;
-  inputNom.name = "nom";
-  inputNom.value = client.nom_client;
-  //label mail
-  let labelMail = create("label", form, "Mail :", null, null);
-  //input mail
-  let inputMail = create("input", form, null, null, null);
-  inputMail.type = "email";
-  inputMail.placeholder = client.mail_client;
-  inputMail.name = "mail_client";
-  inputMail.value = client.mail_client;
-  //label dateNaiss
-  let labelDateNaiss = create("label", form, "Date de naissance :", null, null);
-  //input dateNaiss
-  let inputDateNaiss = create("input", form, null, null, null);
-  inputDateNaiss.type = 'text';
-  inputDateNaiss.placeholder = client.date_naiss_client;
-  inputDateNaiss.name = "date_naissance";
-  inputDateNaiss.value = client.date_naiss_client;
-  //label password
-  let labelPassword = create("label", form, "Mot de passe :", null, null);
-  //input MDP
-  let inputMDP = create("input", form, null, null, null);
-  inputMDP.type = "password";
-  inputMDP.name = "mdp";
-  //création d'un nouveau boutton
-  let buttonValider = create("button", form, null, null, null);
-  buttonValider.type = "Submit";
-=======
   let input = create("input", form, null,null,null);//nessesaire pour le fonctionement du formulaire
   input.type = "hidden";
   input.name = "_token";
@@ -171,7 +109,6 @@ buttonModif.addEventListener('click', function(){
   //création d'un nouveau boutton valider
   let buttonValider = create("button", form, "confirmer modification", "buton", "butonValider");
   buttonValider.type = "submit";
->>>>>>> main
   //buttonModif suppression
   buttonModif.remove();
 
