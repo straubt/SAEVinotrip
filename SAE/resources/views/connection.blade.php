e9e66c7bdb6ae08e874e941f986a4f09a72d4e58
matteosev-IUT <matteo.severini@etu.univ-smb.fr>
2022-12-15 09:03:29 +0000

    ok

--- SAE/resources/views/connection.blade.php
+++ SAE/resources/views/connection.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connection Vinotrip</title> 
+    <title>Connection Vinotrip</title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel="stylesheet"> 
     <link rel="stylesheet" href="css/styleRegister.css">
-	<link rel="stylesheet" href="css/styleGeneral.css">
-	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel="stylesheet">
 	<link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="icon" type="image/x-icon" href="images/images.jpg">
